101d80dd2c82a9fb08c7c6c2b6fd6bc333178aca
Foxyfu <fedosevich.04@mail.ru>
2022-04-13 16:21:00 +0300

    обновлена 26 2

--- 26 1 вариант/2.py
+++ 26 1 вариант/2.py
@@ -4,7 +4,8 @@ import numpy as np
 array = np.random.randint(0, 30, (7, 5))
 print(f'{array} \n ........................')
 
-uniq = np.unique(array, return_counts = True )[1] == 1  
+uniq = np.unique(array, return_counts = True )[1] == 1
+
 
 print(np.unique(array)[uniq])
 
